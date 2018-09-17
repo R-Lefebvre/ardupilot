e9377622fe0aabf38ab65074608f8ac24d01f603
Robert Lefebvre <robert.lefebvre@gmail.com>
2018-09-17 14:54:54 -0400

    Copter: Stable Release. Version to NovaCopter 3.5.1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "NovaCopter V3.5.1-RC2"
+#define THISFIRMWARE "NovaCopter V3.5.1"
 #define FIRMWARE_VERSION 3,5,6,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
