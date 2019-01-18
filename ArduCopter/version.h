1285c0a2907d3314d5fce7ec65e8ce7dfefcd81b
Robert Lefebvre <robert.lefebvre@gmail.com>
2019-01-17 20:18:03 -0500

    Copter: Version to ArduHeli V3.5.7-RL-DEV

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduHeli V3.5.6"
+#define THISFIRMWARE "ArduHeli V3.5.7-RL-DEV"
 #define FIRMWARE_VERSION 3,5,6,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
